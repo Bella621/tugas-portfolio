149d682ccb59f741527b30be0f0854fc66a22703
Bella621 <92489927+Bella621@users.noreply.github.com>
2023-10-16 15:56:54 +0700

    first commit

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,66 @@
+import React from 'react'
+import { Nav, NavLink, NavbarContainer, Span, NavLogo, NavItems, GitHubButton, ButtonContainer, MobileIcon, MobileMenu, MobileNavLogo, MobileLink } from './NavbarStyledComponent'
+import { DiCssdeck } from 'react-icons/di';
+import { FaBars } from 'react-icons/fa';
+import { Bio } from '../../data/constants';
+import { Close, CloseRounded } from '@mui/icons-material';
+import { useTheme } from 'styled-components';
+
+const Navbar = () => {
+  const [isOpen, setIsOpen] = React.useState(false);
+  const theme = useTheme()
+  return (
+    <Nav>
+      <NavbarContainer>
+        <NavLogo to='/'>
+          <a style={{ display: "flex", alignItems: "center", color: "white", marginBottom: '20;', cursor: 'pointer' }}>
+            <DiCssdeck size="3rem" /> <Span>Bell</Span>
+          </a>
+        </NavLogo>
+        <MobileIcon>
+          <FaBars onClick={() => {
+            setIsOpen(!isOpen)
+          }} />
+        </MobileIcon>
+        <NavItems>
+          <NavLink href="/">Home</NavLink>
+          <NavLink href="#about">About</NavLink>
+          <NavLink href='#skills'>Skills</NavLink>
+          <NavLink href='#experience'>Experience</NavLink>
+          <NavLink href='#interest'>Interest</NavLink>
+          <NavLink href='#awards'>Awards</NavLink>
+        </NavItems>
+        <ButtonContainer>
+          <GitHubButton href={Bio.github} target="_blank">Github Profile</GitHubButton>
+        </ButtonContainer>
+        {
+          isOpen &&
+          
+          <MobileMenu isOpen={isOpen}>
+            <MobileLink href="/" onClick={() => {
+              setIsOpen(!isOpen)
+            }}>Home</MobileLink>
+            <MobileLink href="#about" onClick={() => {
+              setIsOpen(!isOpen)
+            }}>About</MobileLink>
+            <MobileLink href='#skills' onClick={() => {
+              setIsOpen(!isOpen)
+            }}>Skills</MobileLink>
+            <MobileLink href='#experience' onClick={() => {
+              setIsOpen(!isOpen)
+            }}>Experience</MobileLink>
+            <MobileLink href='#interest' onClick={() => {
+              setIsOpen(!isOpen)
+            }}>Interest</MobileLink>
+            <MobileLink href='#awards' onClick={() => {
+              setIsOpen(!isOpen)
+            }}>Awards</MobileLink>
+            <GitHubButton style={{padding: '10px 16px',background: `${theme.primary}`, color: 'white',width: 'max-content'}} href={Bio.github} target="_blank">Github Profile</GitHubButton>
+          </MobileMenu>
+        }
+      </NavbarContainer>
+    </Nav>
+  )
+}
+
+export default Navbar
---
+++ src/data/constants.js
@@ -0,0 +1,180 @@
+export const Bio = {
+  name: "Belinda Fatma Cahyani",
+  roles: [
+    "FrontEnd Developer",
+    "Web Developer",
+    "Programmer",
+  ],
+  description:
+    "I am an information engineering student, motivated to continue learning and developing the world of programming, especially in the fields of Web development and data analysis. With the basic knowledge that I have, I am very enthusiastic about continuing to learn about new things in the world of programming. Therefore, I really hope to follow This activity is to increase my experience in the world of web development.",
+  github: "https://github.com/Bella621",
+  resume:
+    "https://drive.google.com/file/d/1bBHKij9GCWLkjfuctb4uYD_T5TSdhxsh/view?usp=drive_link",
+  linkedin: "https://www.linkedin.com/in/belinda-fatma-cahyani/",
+  twitter: "https://x.com/belindaftm4?t=LX63H7sWFW3xASLeVRjvCA&s=09",
+  insta: "https://instagram.com/belindaftm4?igshid=OGQ5ZDc2ODk2ZA==",
+  
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React Js",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "Redux",
+        image:
+          "https://d33wubrfki0l68.cloudfront.net/0834d0215db51e91525a25acf97433051f280f2f/c30f5/img/redux.svg",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png",
+      },
+    ],
+  },
+  {
+    title: "Others",
+    skills: [
+      {
+        name: "Git",
+        image:
+          "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Canva",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/0/08/Canva_icon_2021.svg",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Java",
+        image:
+          "https://logowik.com/content/uploads/images/731_java.jpg",
+      },
+      {
+        name: "Figma",
+        image:
+          "https://camo.githubusercontent.com/ed93c2b000a76ceaad1503e7eb9356591b885227e82a36a005b9d3498b303ba5/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6669676d612f6669676d612d69636f6e2e737667",
+      },
+      {
+        name: "Adobe Photoshop",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Adobe_Photoshop_CC_icon.svg/640px-Adobe_Photoshop_CC_icon.svg.png",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://asset.brandfetch.io/idK1RDMSRY/idPamGvnsf.jpeg?updated=1692337555498",
+    role: "Android Developer",
+    company: "Program Baparekraf Digital Talent 2023 dari Dicoding",
+    date: "2 Mei 2023 - 21 Juli 2023",
+    desc: "Memulai Pembelanjaran Dengan Kotlin.",
+    skills: [
+      "Kotlin",
+    ],
+  },
+  {
+    id: 1,
+    img: "https://trainingeltasa.com/wp-content/uploads/2021/07/android-studio-logo-3.png",
+    role: "Android Studio",
+    company: "Muria Kudus University",
+    date: "Semester IV",
+    desc: "Mempelajari pemrograman mobile dengan java dan membuat project uas dengan membuat aplikasi pembelajaran huruf hijaiyah",
+    skills: [
+      "Java",
+    ],
+  },
+  {
+    id: 2,
+    img: "https://upload.wikimedia.org/wikipedia/commons/2/2c/Visual_Studio_Icon_2022.svg",
+    role: "Visual Studio Basic",
+    company: "Muria Kudus University",
+    date: "Semester IV",
+    desc: "Mempelajari praktikum sistem basis data dengan Visual Basic .NET dan membuat project uas dengan membuat web jadwal mata kuliah",
+    skills: [
+      "Visual Basic .NET"
+    ],
+  },
+];
+
+export const awards = [
+  {
+    id: 0,
+    img: "https://i.ibb.co/XVkc19J/sertifikat-course-80-2291523-170723071747-1-pages-to-jpg-0001.jpg",
+    school: "Program Baparekraf Digital Talent 2023 dari Dicoding",
+    date: " 2 Mei - 21 Juli 2023 ",
+    grade: "BAIK",
+    desc: "I have successfully completed the Baparekraf Digital Talent 2023 program from Dicoding with great excellence.",
+    degree: "Android Developer Dengan Kotlin",
+  },
+  {
+    id: 1,
+    img: "https://i.ibb.co/9G84G7V/sertifikat.jpg",
+    school: "Praktek Kerja Lapangan",
+    date: "17 Desember 2018 - 17 Mei 2019",
+    grade: "BAIK",
+    desc: "I successfully completed my internship at BMT Mualamat Mulia from 2018 to 2019. During this period, I gained valuable hands-on experience and a deeper understanding of the financial industry. It was a fulfilling and educational journey that allowed me to apply my theoretical knowledge to real-world situations, and I am grateful for the opportunity to have been a part of the BMT Mualamat Mulia team during that time.",
+    degree: "SMK N 1 KUDUS, Di BMT Mualamat Mulia",
+  },
+];
+
+export const interest = [
+  {
+    id: 1,
+    title: "Watching Korean Movie",
+    date: "2020 - now",
+    description:
+      "My most enjoyable hobby is watching Korean dramas. Every time I watch episodes filled with emotions and deep stories, I feel as if I am immersed in the narrative. Strong characters and intricate plots always make me eager to find out what will happen next. Moreover, Korean dramas often carry profound moral messages that can provide inspiration and new perspectives in daily life. For me, watching Korean dramas is not just entertainment but also a deep and inspiring emotional experience.",
+    image:
+      "https://i.ibb.co/tmMxWk3/72d8e8426947f6ef73b88050177e3c36.jpg",
+    tags: [],
+    
+  },
+  {
+    id: 2,
+    title: "Cooking",
+    date: "from childhood - now",
+    description:
+      "I have a profound passion for cooking. For me, cooking is not just a daily chore but rather an art that captivates both the heart and the palate. When I'm in the kitchen, I feel like an artist creating culinary masterpieces with a variety of ingredients. Each spice and vegetable plays a crucial role in achieving the perfect flavor harmony. From preparing simple everyday meals to crafting special dishes for special occasions, cooking brings me unparalleled happiness and satisfaction. It is a hobby that not only delights the taste buds but also reflects my creative expression in creating delicious and fulfilling cuisine.",
+    image:
+      "https://i.ibb.co/178K0m2/548744c4349d6bac09e7666745aacc97.jpg",
+    tags: [],
+  },
+  {
+    id: 4,
+    title: "Listening to music",
+    date: "from childhood - now",
+    description:
+      "I thoroughly enjoy my hobby of listening to music. To me, music is the best way to relax and alleviate stress. Every time I play my favorite music, the world seems to pause for a moment, and I become engrossed in the melodies and lyrics that play. Through music, I feel I can express feelings and emotions that are difficult to put into words. This hobby has become a crucial part of my life, providing tranquility and happiness in every note I listen to.",
+    image:
+      "https://i.ibb.co/SnGhKw8/81a5112ebe675d623a325431fbd9c4ce.jpg",
+    tags: [],
+  },
+];
